67e0571363871ff1864134f8a41d19de6bc1d4a7
akshayyborse <borseakshay128@gmail.com>
2024-12-22 19:22:11 +0530

    Update Contact.tsx

--- app/contact-section/Contact.tsx
+++ app/contact-section/Contact.tsx
@@ -6,7 +6,7 @@ import AnimatedBody from "../animations/AnimatedBody";
 import AnimatedTitle from "../animations/AnimatedTitle";
 import AnimatedWords2 from "../animations/AnimatedWords2";
 import { motion } from "framer-motion";
-// import heartIcon from "../../public/heart icon.png";
+import heartIcon from "../../public/heart icon.png";
 
 const Contact = () => {
   return (
@@ -26,11 +26,11 @@ const Contact = () => {
               "flex max-w-[500px] flex-col items-start text-left text-[150px] font-extrabold uppercase leading-[0.9em] text-[#e4ded7] sm:max-w-full sm:flex-row sm:items-center sm:justify-center sm:text-center sm:text-[170px] md:text-[200px] lg:text-center lg:text-[270px] xl:text-[390px]"
             }
           />
-          {/* <Image
+          <Image
             src={heartIcon}
             alt="Heart Icon"
             className="heartbeat md:-bottom-18 absolute -bottom-5 left-64 w-[120px] sm:-bottom-14 sm:left-[40%] md:left-[40%] md:w-[150px] lg:-bottom-16 lg:left-[42%] lg:w-[230px]"
-          /> */}
+          />
         </div>
 
         <div className="mt-20 flex w-full flex-col items-end justify-center gap-16 sm:mt-32 sm:gap-12 md:mt-40 md:flex-row md:items-start md:justify-between lg:mt-12 lg:max-w-[1440px]">
@@ -45,7 +45,7 @@ const Contact = () => {
             />
             <div className="bor der mt-5 flex w-[298px] items-center gap-1 md:w-[335px] md:gap-2.5">
               <Link
-                href="mailto:company@oceanmonks.in?subject=Lets%20work%20together!&amp;body=Hello%2C%20I%20think%20we%20need%20you%20to%20work%20on%2Fcollaborate%20this%20particular%20product...%20Reach%20out%20as%20soon%20as%20you%20can."
+                href="mailto:victor.will.chuk@gmail.com?subject=Lets%20work%20together!&amp;body=Hello%2C%20I%20think%20we%20need%20you%20to%20work%20on%2Fcollaborate%20this%20particular%20product...%20Reach%20out%20as%20soon%20as%20you%20can."
                 target="_blank"
                 aria-label="Send me an email"
                 className="bor der mt-1 w-[147px] flex-1 underline underline-offset-2 hover:no-underline sm:mt-2 sm:w-[170px] md:mt-3 lg:mt-4"
@@ -62,7 +62,7 @@ const Contact = () => {
                 }
               />
               <Link
-                href="https://cal.com/team-oceanmonks/30min"
+                href="https://cal.com/akshay-borse/30min"
                 target="_blank"
                 aria-label="Send me an email"
                 className="bor der mt-1 w-[110px] flex-1 underline underline-offset-2 hover:no-underline sm:mt-2 sm:w-[147px] md:mt-3 md:-ml-[3px] md:w-[120px] lg:mt-4"
@@ -76,7 +76,7 @@ const Contact = () => {
           </div>
 
           <div className="flex gap-10 text-[16px] font-bold text-[#e4ded7]  sm:gap-14 sm:text-[24px] md:gap-10 md:text-[16px] lg:gap-20 lg:text-[28px]">
-            {/* <Link
+            <Link
               href="https://github.com/akshayyborse"
               target="_blank"
               aria-label="View GitHub Profile"
@@ -89,9 +89,9 @@ const Contact = () => {
                 wordSpace={"mr-[0.25em]"}
                 charSpace={"mr-[0.01em]"}
               />
-            </Link> */}
+            </Link>
             <Link
-              href="https://www.linkedin.com/in/ocean-monks-0b7799323/"
+              href="https://www.linkedin.com/in/akshayyborse/"
               target="_blank"
               aria-label="View LinkedIn Profile"
             >
@@ -104,7 +104,7 @@ const Contact = () => {
                 charSpace={"mr-[0.01em]"}
               />
             </Link>
-            {/* <Link
+            <Link
               href="https://twitter.com/akshayyborse"
               target="_blank"
               aria-label="View Twitter Profile"
@@ -131,14 +131,14 @@ const Contact = () => {
                 wordSpace={"mr-[0.25em]"}
                 charSpace={"mr-[0.01em]"}
               />
-            </Link> */}
+            </Link>
             <Link
               href="https://oceanmonks.in/"
               target="_blank"
               aria-label="View Website"
             >
               <AnimatedTitle
-                text={"US"}
+                text={"HN"}
                 className={
                   "text-[16px] font-bold text-[#e4ded7] sm:text-[20px] md:text-[16px] lg:text-[28px]"
                 }
