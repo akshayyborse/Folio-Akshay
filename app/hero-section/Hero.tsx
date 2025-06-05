b21704fc5b824a88eeca646b590a09646ddd8844
Akshayyborse <borseakshay128@gmail.com>
2025-06-05 15:49:31 +0530

    Update Hero.tsx

--- app/hero-section/Hero.tsx
+++ app/hero-section/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
       <div className="absolute top-10 flex justify-between sm:w-[90%] lg:max-w-[1440px]">
         <div>
           <Link
-            href="https://drive.google.com/file/d/152uBuZ5bCeg-9C6jAZq2F1McQBUA-3Ga/view?usp=drive_link/view"
+            href="https://drive.google.com/file/d/17c8ScPanN-rPubE8J8uKPwd9x6AG0rtx/view"
             target="_blank" 
             aria-label="VIEW RESUME"
           >
