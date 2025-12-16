520cf1c3f4b78cae71a3c32f3185874c598a6ede
Akshay Borse <borseakshay128@gmail.com>
2025-12-16 23:26:31 +0530

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 const NotFound = () => {
   return (
     <div className="flex h-[100vh] flex-col items-center justify-center bg-[#0E1016] text-[#e4ded7] gap-5">
-      <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] uppercase">Egbon, you don miss road oh!</p>
+      <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] uppercase">The page you are looking for does not exist.</p>
       <Image
-        src="https://user-images.githubusercontent.com/84178696/228884970-66a9687c-b364-48e4-91ca-05332dadbba9.png"
-        alt="Funny Meme"
+        src="https://github.com/akshayyborse/akshayyborse/blob/34883b2c3b5a771f7e5af8d52fa546870fe30996/notfound.gif"
+        alt="Not Found"
         width={858}
         height={483}
         className="w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%]"
