4dc95b68bf235cb67caf8211656635d76c9a852f
Akshay Borse <borseakshay128@gmail.com>
2025-12-16 23:28:29 +0530

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,26 +1,29 @@
-import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-const NotFound = () => {
+export default function NotFound(): JSX.Element {
   return (
-    <div className="flex h-[100vh] flex-col items-center justify-center bg-[#0E1016] text-[#e4ded7] gap-5">
-      <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] uppercase">The page you are looking for does not exist.</p>
+    <div className="flex h-screen flex-col items-center justify-center bg-[#0E1016] text-[#e4ded7] gap-5">
+      <p className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] uppercase text-center">
+        The page you are looking for does not exist.
+      </p>
+
       <Image
-        src="https://github.com/akshayyborse/akshayyborse/blob/34883b2c3b5a771f7e5af8d52fa546870fe30996/notfound.gif"
+        src="https://raw.githubusercontent.com/akshayyborse/akshayyborse/34883b2c3b5a771f7e5af8d52fa546870fe30996/notfound.gif"
         alt="Not Found"
         width={858}
         height={483}
         className="w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%]"
+        priority
       />
-      <div>
-        <Link href="/" className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] uppercase">
-          Head back to my{" "}
-          <span className="underline underline-offset-2">Portfolio</span>
-        </Link>
-      </div>
+
+      <Link
+        href="/"
+        className="text-[25px] sm:text-[30px] md:text-[35px] lg:text-[40px] uppercase"
+      >
+        Head back to my{" "}
+        <span className="underline underline-offset-2">Portfolio</span>
+      </Link>
     </div>
   );
-};
-
-export default NotFound;
+}
